83d4a520e82fa9b2adeeada89bc61f2ec5d4ed46
berger <robertberger.dev@gmail.com>
2022-12-30 19:08:47 -0500

    added readyState property

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ myRequest.open('GET', 'http://www.boredapi.com/api/activity', true);
 myRequest.send();
 
 myRequest.onreadystatechange = function() {
-    //
+    if (this.readyState == 4) {
+        // Response was received
+    } else {
+        // Not ready yet
+    }
 }
