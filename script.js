4725332a26a678b2d4348de800950ef65512e2f3
berger <robertberger.dev@gmail.com>
2022-12-30 19:10:41 -0500

    checking to make sure the HTTP status was successful

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let myRequest = new XMLHttpRequest();
 myRequest.open('GET', 'http://www.boredapi.com/api/activity', true);
 myRequest.send();
 
-myRequest.onreadystatechange = function() {
+myRequest.onreadystatechange = function(){
     if (this.readyState == 4) {
-        // Response was received
+        if (this.status == 200) {
+            // Response was received and was successful
+        } else {
+            // Reponse was received but there might have been a problem
+        }
     } else {
         // Not ready yet
     }
