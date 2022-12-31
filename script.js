06bf9480c2701ffbcbebe771e73dc12e30726c3e
berger <robertberger.dev@gmail.com>
2022-12-30 19:38:21 -0500

    final code example

--- script.js
+++ script.js
@@ -5,8 +5,27 @@ myRequest.send();
 myRequest.onreadystatechange = function(){
     if (this.readyState == 4) {
         if (this.status == 200) {
-            console.log(JSON.parse(this.responseText));
-        } else{
+            // Parse the response to a variable
+            let myResponse = JSON.parse(this.responseText);
+            
+            // Create a div container element
+            let myDiv = document.createElement('div');
+
+            // Create a paragraph element and add the innerHTML
+            let activityTag = document.createElement('p');
+            activityTag.innerHTML = "Activity Name: " + myResponse.activity;
+
+            // Create another paragraph element and add the innerHTML
+            let participantsTag = document.createElement('p');
+            participantsTag.innerHTML = "Number of Participants: " + myResponse.participants;
+
+            // Append the paragraph tags to the created div element
+            myDiv.appendChild(activityTag);
+            myDiv.appendChild(participantsTag);
+
+            // Add the div element to the body of the document.
+            document.body.appendChild(myDiv);
+        } else {
             console.log("Error processing Request")
         }
     }
